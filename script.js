3f6e32f77662887772a8c4207a87b8fa47cfa101
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-05-20 17:23:17 +0200

    Explication de l'algorithme quick sort

--- script.js
+++ script.js
@@ -26,3 +26,12 @@ console.log(
     1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
   ])
 ); // [ 1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643]
+
+// EXPLICATION DU CODE JAVASCRIPT
+
+/**
+ * Etape 1 : Création de la fonction quickSort ayant comme paramètre un tableau.
+ * Etape 2 : Choisir un élément pivot dans le tableau.
+ * Etape 3 : Diviser le tableau en deux sous-tableaux, l'un contenant des éléments plus petits que le pivot et l'autre contenant des éléments plus grands que le pivot.
+ * Etape 4 : Appliquer de manière récursive l’algorithme de tri rapide aux deux sous-tableaux jusqu’à ce que le tableau entier soit trié.
+ */
